5a4ed5be6302382240b54fe788fb682326ca545d
Ssantiago8 <54732915+Ssantiago8@users.noreply.github.com>
2024-01-26 15:26:48 -0500

    Backend .env

--- database/database.js
+++ database/database.js
@@ -1,6 +1,12 @@
 import { Sequelize } from "sequelize";
+import {} from "dotenv/config";
 
-export const sequelize = new Sequelize("notes", "postgres", "mypassword", {
-  host: "localhost",
-  dialect: "postgres",
-});
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
